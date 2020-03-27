6613ef3334188ffe3c9b28d6039b945d8abab8a0
mfasano727 <60932709+mfasano727@users.noreply.github.com>
2020-03-27 03:24:35 -0500

    Add files via upload

---
+++ MBDyn_guitools/MBDynFreeCAD.py
@@ -0,0 +1,167 @@
+
+
+import os
+import sys
+from PySide2 import QtCore, QtGui, QtWidgets
+import FreeCADGui as Gui
+import FreeCAD as App
+import MBDyn_locator
+MBDwbPath = os.path.dirname(MBDyn_locator.__file__)
+MBDwb_icons_path = os.path.join(MBDwbPath, 'icons')
+from  MBDyn_utilities.MBDyn_funcs import *
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(30, 20, 91, 17))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(30, 50, 41, 17))
+        self.label_2.setObjectName("label_2")
+        self.install_path = QtWidgets.QLineEdit(Form)
+        self.install_path.setGeometry(QtCore.QRect(80, 50, 191, 25))
+        self.install_path.setObjectName("install_path")
+        self.fpInstallpath = QtWidgets.QPushButton(Form)
+        self.fpInstallpath.setGeometry(QtCore.QRect(270, 50, 89, 25))
+        self.fpInstallpath.setObjectName("fpInstallpath")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(30, 100, 91, 17))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(30, 130, 51, 17))
+        self.label_4.setObjectName("label_4")
+        self.out_filename = QtWidgets.QLineEdit(Form)
+        self.out_filename.setGeometry(QtCore.QRect(80, 130, 191, 25))
+        self.out_filename.setObjectName("out_filename")
+        self.fp_outfile = QtWidgets.QPushButton(Form)
+        self.fp_outfile.setGeometry(QtCore.QRect(270, 160, 89, 25))
+        self.fp_outfile.setObjectName("fp_outfile")
+        self.out_location = QtWidgets.QLineEdit(Form)
+        self.out_location.setGeometry(QtCore.QRect(80, 160, 191, 25))
+        self.out_location.setObjectName("out_location")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(30, 160, 41, 17))
+        self.label_5.setObjectName("label_5")
+        self.runSim = QtWidgets.QPushButton(Form)
+        self.runSim.setGeometry(QtCore.QRect(40, 200, 291, 25))
+        self.runSim.setObjectName("write input")
+        self.stopSim = QtWidgets.QPushButton(Form)
+        self.stopSim.setGeometry(QtCore.QRect(40, 230, 291, 25))
+        self.stopSim.setObjectName("stopSim")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(140, 260, 101, 25))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+
+        self.fpInstallpath.clicked.connect(self.setinstallpath)
+        self.fp_outfile.clicked.connect(self.setoutputpath)
+        self.runSim.clicked.connect(self.runSimulation)
+        self.stopSim.clicked.connect(self.stopSimulation)
+        self.pushButton.clicked.connect(self.outputMessage)
+        
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Run Simulation"))
+        self.label.setText(_translate("Form", "MBDyn Path:"))
+        self.label_2.setText(_translate("Form", "Path:"))
+        self.fpInstallpath.setText(_translate("Form", "Browse..."))
+        self.label_3.setText(_translate("Form", "Output file:"))
+        self.label_4.setText(_translate("Form", "Name:"))
+        self.fp_outfile.setText(_translate("Form", "Browse..."))
+        self.label_5.setText(_translate("Form", "Path:"))
+        self.runSim.setText(_translate("Form", "Run Simulation"))
+        self.stopSim.setText(_translate("Form", "Stop Simulation"))
+        self.pushButton.setText(_translate("Form", "View status"))
+        self.stopSim.setEnabled(False)
+        self.pushButton.setEnabled(False)
+	
+	
+
+    def setinstallpath(self):
+        mbdyninstallpath = QtWidgets.QFileDialog.getOpenFileName()[0]
+        self.install_path.setText(mbdyninstallpath)
+
+    def setoutputpath(self):
+        outlocation = QtWidgets.QFileDialog.getExistingDirectory()
+#        outlocation = QDir.toNativeSeparators(outlocation)
+        if os.sep=='\\':
+            outlocation=outlocation.replace('/', '\\')
+        self.out_location.setText(outlocation)
+        self.out_filename.setText("input.txt")
+        App.Console.PrintMessage("filename\n")
+    def runSimulation(self):
+        App.Console.PrintMessage("filename2 \n")
+        import os
+        import subprocess
+        
+#        mbdyn = os.path.realpath(self.install_path.text())
+#        outfile = os.path.realpath(self.out_location.text() + "/" + self.out_filename.text())
+        outfile = self.out_location.text()
+#        App.Console.PrintMessage("filename" + outfile + "\n")
+        if os.path.isdir(outfile):
+#            App.Console.PrintMessage("filename" + outfile + "\n")
+            outfile = os.path.join(outfile , self.out_filename.text())
+        
+            App.Console.PrintMessage("filename" + outfile + "\n")
+            writeInputFile(outfile)
+        else: 
+            App.Console.PrintMessage("filename" + outfile + " not a dir\n")
+        '''
+#        Change the file path here
+        infile = os.path.realpath('/home/adityabhagat/Documents/projects/long')
+        self.outlogfile = self.out_location.text() + "/" + "out_msg.log"
+        self.ol = open(self.outlogfile, 'w')
+        args = [mbdyn, '-f', infile, '-o', outfile]
+        self.p = subprocess.Popen(args, \
+                  #env=env_vars, \
+                  stdout = self.ol, \
+                  stderr = subprocess.STDOUT,
+                  universal_newlines=True)
+        self.runSim.setEnabled(False)
+        self.stopSim.setEnabled(True)
+        self.pushButton.setEnabled(True)
+        '''
+    def stopSimulation(self):
+        self.p.terminate()
+        self.stopSim.setEnabled(False)
+        self.pushButton.setEnabled(True)
+	
+    def outputMessage(self):
+        self.ol.close()
+        self.olr = open(self.outlogfile, 'r')
+        outmessage = self.olr.read()
+        popupdlg = QtWidgets.QMessageBox.information(Form, 'output message', outmessage)
+	
+	
+	
+class mbdyn_launchGui(QtWidgets.QDialog,  Ui_Form):	
+    def __init__(self):
+        super(mbdyn_launchGui, self).__init__()
+        self.setupUi(self)
+    def GetResources(self):
+        return {'Pixmap': os.path.join(MBDwb_icons_path, 'WrtMBDynIcon.svg'),
+                'MenuText': "MBD write input",
+                'ToolTip': "write MBDyn input dialog"}
+    def Activated(self):
+        """Do something here"""
+#        App.Console.PrintMessage( Gui.activeWorkbench().iv.initial_time)
+        self.show()
+        
+Gui.addCommand('mbdyn_launchGui', mbdyn_launchGui())	
+'''
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    
+    sys.exit(app.exec_())
+'''
---
+++ MBDyn_guitools/ref_cmd.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# ref_cmd.py
+
+import os
+import sys
+import MBDyn_locator
+MBDwbPath = os.path.dirname(MBDyn_locator.__file__)
+MBDwb_icons_path = os.path.join(MBDwbPath, 'icons')
+import MBDyn_objects.model_so
+from PySide2 import QtCore, QtGui, QtWidgets
+import FreeCAD as App
+import FreeCADGui as Gui
+
+
+
+from   MBDyn_guitools.dia_ref import Ui_ref_dialog
+
+class ref_cmd(QtWidgets.QDialog, Ui_ref_dialog):
+    """MBD create reference command"""
+    def __init__(self):
+        super(ref_cmd, self).__init__()
+        self.setupUi(self)
+
+    def GetResources(self):
+        return {'Pixmap': os.path.join(MBDwb_icons_path, 'reference.svg'),
+                'MenuText': "create reference command",
+                'ToolTip': "input reference parameters"}
+    def Activated(self):
+        """Do something here"""
+        self.parent_ref_box.clear()
+        self.parent_ref_box.addItem("none")
+        if App.ActiveDocument.getObjectsByLabel('References') != None :
+            for refobj in App.ActiveDocument.References.Group:
+                self.parent_ref_box.addItem(refobj.ref_name)
+
+        self.pos_x.setText("0") ; self.pos_y.setText("0") ; self.pos_z.setText("0")
+        self.vect1_x.setText("1") ; self.vect1_y.setText("0") ; self.vect1_z.setText("0")
+        self.vect2_x.setText("0") ; self.vect2_y.setText("1") ; self.vect2_z.setText("0")
+        self.vect3_x.setText("0") ; self.vect3_y.setText("0") ; self.vect3_z.setText("1")
+        self.vel_x.setText("0") ; self.vel_y.setText("0") ; self.vel_z.setText("0")
+        self.ang_vel_x.setText("0") ; self.ang_vel_y.setText("0") ; self.ang_vel_z.setText("0")
+
+        self.OM_type.currentIndexChanged.connect(self.set_OM_type)
+#        self.valid = QtGui.QDoubleValidator()
+#        self.pos_x.setValidator(self.valid)
+#        self.pos_x.textEdited.connect(self.check_valid())
+#        self.pos_x.textEdited.emit(self.pos_x.text())
+        self.show()
+
+        App.Console.PrintMessage(" Activated: " + "\n")
+        
+        return
+
+    def IsActive(self):
+        if App.ActiveDocument == None:
+            return False
+        else:
+            return True
+
+
+    def accept(self): 
+        index = self.OM_type.currentIndex()
+        App.Console.PrintMessage(" property1: " +self.OM_type.itemText(index) + "\n")
+#        testgroup  = App.ActiveDocument.getObjectsByLabel('references')
+#        new_ref = App.ActiveDocument.addObject("App::FeaturePython","reference2")
+        new_ref =App.ActiveDocument.References.newObject("App::FeaturePython","reference" + str(self.parent_ref_box.count()))
+        MBDyn_objects.model_so.MBDynReference(new_ref)
+        new_ref.ViewObject.Proxy = 0
+
+        new_ref.ref_label = self.parent_ref_box.count()
+        new_ref.ref_name = "reference" + str(self.parent_ref_box.count())
+        # set the reference to be refered to
+        if self.parent_ref_box.currentIndex() == 0:
+            new_ref.refered_label = 0
+        else:
+            for refobj in App.ActiveDocument.References.Group:
+                if refobj.ref_name  == self.parent_ref_box.currentText():
+                    new_ref.refered_label = refobj.ref_label
+        new_ref.position = App.Vector(float(self.pos_x.text()), float(self.pos_y.text()), float(self.pos_z.text()))
+        new_ref.orientation = [App.Vector(float(self.vect1_x.text()), float(self.vect1_y.text()), float(self.vect1_z.text())), 
+                               App.Vector(float(self.vect2_x.text()), float(self.vect2_y.text()), float(self.vect2_z.text())), 
+                               App.Vector(float(self.vect3_x.text()), float(self.vect3_y.text()), float(self.vect3_z.text()))]
+        new_ref.orientation_des = self.OM_type.currentText()
+        new_ref.vel = App.Vector(float(self.vel_x.text()), float(self.vel_y.text()), float(self.vel_z.text()))
+        new_ref.ang_vel = App.Vector(float(self.ang_vel_x.text()), float(self.ang_vel_y.text()), float(self.ang_vel_z.text()))
+
+    def set_OM_type(self):
+        index = self.OM_type.currentIndex()
+        App.Console.PrintMessage(" property: " +self.OM_type.itemText(index) + "\n")
+        if self.OM_type.currentIndex() == 0:  #xy selected  only vectors 1 and 2 are editable
+            self.vect1_x.setVisible(True);  self.vect1_y.setVisible(True);  self.vect1_z.setVisible(True)
+            self.vect2_x.setVisible(True);  self.vect2_y.setVisible(True);  self.vect2_z.setVisible(True)
+            self.vect3_x.setVisible(False); self.vect3_y.setVisible(False); self.vect3_z.setVisible(False)
+        elif self.OM_type.currentIndex() == 1:  #xz selected  only vectors 1 and 3 are editable
+            self.vect1_x.setVisible(True);  self.vect1_y.setVisible(True);  self.vect1_z.setVisible(True)
+            self.vect2_x.setVisible(False); self.vect2_y.setVisible(False); self.vect2_z.setVisible(False)
+            self.vect3_x.setVisible(True);  self.vect3_y.setVisible(True);  self.vect3_z.setVisible(True)
+        elif self.OM_type.currentIndex() == 2:  #yz selected  only vectors 2 and 3 are editable
+            self.vect1_x.setVisible(False); self.vect1_y.setVisible(False); self.vect1_z.setVisible(False)
+            self.vect2_x.setVisible(True);  self.vect2_y.setVisible(True);  self.vect2_z.setVisible(True)
+            self.vect3_x.setVisible(True);  self.vect3_y.setVisible(True);  self.vect3_z.setVisible(True)
+        elif self.OM_type.currentIndex() == 3:  #matr selected  all vectors are editable
+            self.vect1_x.setVisible(True);  self.vect1_y.setVisible(True);  self.vect1_z.setVisible(True)
+            self.vect2_x.setVisible(True);  self.vect2_y.setVisible(True);  self.vect2_z.setVisible(True)
+            self.vect3_x.setVisible(True);  self.vect3_y.setVisible(True);  self.vect3_z.setVisible(True)
+        else:  # for euler only  vector 1 is editable
+            self.vect1_x.setVisible(True);  self.vect1_y.setVisible(True);  self.vect1_z.setVisible(True)
+            self.vect2_x.setVisible(False); self.vect2_y.setVisible(False); self.vect2_z.setVisible(False)
+            self.vect3_x.setVisible(False); self.vect3_y.setVisible(False); self.vect3_z.setVisible(False)
+
+    def check_valid(self):
+         pass
+#        teststr = self.sender()
+#        if self.valid.validate(teststr.text(),0) != QtGui.QValidator.Acceptable:
+#           self.sender.setText("0.0")
+
+Gui.addCommand('ref_cmd', ref_cmd())
