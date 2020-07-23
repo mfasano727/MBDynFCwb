144b81e177e1287c3186b58427ece551fdac22b2
T-Garnier <67495766+T-Garnier@users.noreply.github.com>
2020-07-23 00:04:57 -0400

    Initial commit

---
+++ MBDyn_utilities/constants.py
@@ -0,0 +1,7 @@
+
+# settings constancts
+WB_USER_SETTINGS = "User parameter:BaseApp/Preferences/Mod/Mbdyn"
+SOLVERS_USER_SETTINGS = WB_USER_SETTINGS + "/Solvers"
+GENERAL_USER_SETTINGS = WB_USER_SETTINGS + "/General"
+SEP = ";"
+MBDYN_EXTENSIONS = ['.mbd', '.mbdyn', '.mdl', '.set', '.ref', '.nod', '.elm']
