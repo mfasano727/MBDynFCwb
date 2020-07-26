45b333dd77ed5986a860b0388dbd523097c1e116
mfasano727 <60932709+mfasano727@users.noreply.github.com>
2020-07-26 16:23:52 -0500

    Add files via upload

--- InitGui.py
+++ InitGui.py
@@ -6,8 +6,6 @@ import FreeCADGui as Gui
 import MBDyn_locator
 MBDwbPath = os.path.dirname(MBDyn_locator.__file__)
 MBDwb_icons_path = os.path.join(MBDwbPath, 'icons')
-
-
 class MbdynGui(Workbench):
 
     def __init__(self):
@@ -44,7 +42,6 @@ class MbdynGui(Workbench):
         self.__class__.MenuText = "MBDyn"
         self.__class__.ToolTip = "Model for Mbdyn simulation"
 
-
     def GetClassName(self):
         return "Gui::PythonWorkbench"
 
@@ -66,15 +63,15 @@ class MbdynGui(Workbench):
         import MBDyn_guitools.postproc_AS4_cmd
         import MBDyn_guitools.total_joint_cmd
         import MBDyn_guitools.total_pinjoint_cmd
+        import MBDyn_guitools.inline_joint_cmd
         import MBDyn_guitools.ramp_drive_cmd
         import MBDyn_guitools.axial_rot_joint_AS4_cmd
-        
+
         from MBDyn_settings.wdgt_solver_settings import wdgt_solver_settings
-  
         self.list = ["mbdyn_configure", "mbdyn_launchGui", "body_sel_cmd",
                     "ref_cmd", "struct_node_cmd", "revpin_joint_cmd",
                     "hinge_joint_cmd", "total_joint_cmd", "total_pinjoint_cmd",
-                    "axial_rot_joint_cmd", "ramp_drive_cmd", "postproc_cmd"]
+                    "axial_rot_joint_cmd", "inline_joint_cmd", "ramp_drive_cmd", "postproc_cmd"]
         self.appendToolbar("Mbdyn_comands", self.list)
         self.appendMenu("Mbdyn_menu", self.list)
         Log("Loading MyModule... done\n")
@@ -86,9 +83,9 @@ class MbdynGui(Workbench):
         # The commande Gui.addIcon gives me the following warning (on freecad 0.19):
         # <string>:86: DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats
         Gui.addIcon("preferences-mbdyn", img_path)
-
+                
     def Activated(self):
-        App.Console.PrintMessage("MBDyn Workbench Activated")
+        App.Console.PrintMessage("test")
 
 
     def Deactivated(self):
