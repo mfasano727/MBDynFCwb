b1ceeadb1b0ed2b12b1ee1715ee59f625a6ac95d
mfasano727 <60932709+mfasano727@users.noreply.github.com>
2020-10-28 18:09:32 -0500

    Add files via upload

---
+++ MBDyn_guitools/Commands/gravity_cmd.py
@@ -0,0 +1,37 @@
+import os
+
+import FreeCAD as App
+import FreeCADGui as Gui
+
+import MBDyn_locator
+MBDwbPath = os.path.dirname(MBDyn_locator.__file__)
+MBDwb_icons_path = os.path.join(MBDwbPath, 'icons')
+
+from MBDyn_guitools.External_Ui.gravity_field import GravityField
+
+class CommandAddGravity:
+    """Command to create a new gravity field"""
+
+    def GetResources(self):
+        return {'Pixmap': "", #os.path.join(MBDwb_icons_path, 'MBDyn_AddSim_cmd.svg'),
+                'MenuText': "Add Gravity",
+                'ToolTip': "Add a gravity field to the model"}
+
+    def IsActive(self):
+        if App.ActiveDocument is None:
+            return False
+        elif hasattr(App.ActiveDocument, "MBDyn_Workbench"):
+            return True
+        else:
+            return False
+
+    def Activated(self):
+        #Call the Gui
+        gravObj = None
+        if hasattr(App.ActiveDocument, "GravityField"):
+            gravObj = getattr(App.ActiveDocument, "GravityField")
+        self.ui = GravityField(gravObj)
+        self.ui.show()
+
+
+Gui.addCommand('CommandAddGravity', CommandAddGravity())
