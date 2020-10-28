fddc9d3c97011de1d700cd0c3d179962af5874d7
mfasano727 <60932709+mfasano727@users.noreply.github.com>
2020-10-28 18:47:36 -0500

    Add files via upload

---
+++ MBDyn_utilities/setup_data/MBDyn_problems/problem.py
@@ -0,0 +1,7 @@
+class MBDynProblem():
+    def __init__(self, name):
+        """ This is a template class, should not be used directly"""
+        self.name = name
+
+    def write(self):
+        pass
