e906de8cc70f0745d6d6a5f3aff352d68cdecfa5
mfasano727 <60932709+mfasano727@users.noreply.github.com>
2020-11-07 09:33:26 -0500

    Add files via upload

--- MBDyn_utilities/place_funcs.py
+++ MBDyn_utilities/place_funcs.py
@@ -1,7 +1,7 @@
 import FreeCAD as App
 import MBDyn_objects.MBDynJoints
 import MBDyn_objects.model_so
-
+from math import pi, sqrt
 
 def calc_placement(pos, orient, orient_des):
     '''calculate FreeCAD placement from MBDyn position and orientation matrix'''
@@ -76,3 +76,22 @@ def calc_orient_Z(zdir,odir = None):
         ang = zir.getAngle(odir)
         if ang < 0.0000001:
             return [odir.normalize(),App.Vector(0,0,0),  zdir.normalize()]
+
+
+def near_point_on_line(Pl1, Pl2, Pn):  #pass 3 App.vectors
+    ''' this finds the point on a line defined by Pl1 and Pl2 nearest to a point Pn not on the line
+        https://arstechnica.com/civis/viewtopic.php?f=26&t=149128 is where the algorithm can e found'''
+    u = (Pn.x - Pl1.x)*(Pl2.x - Pl1.x) + (Pn.y - Pl1.y)*(Pl2.y - Pl1.y) + (Pn.z - Pl1.z)*(Pl2.z - Pl1.z)
+    # get distance bbetween Pl1 and Pl2
+    dist = distance(Pl1, Pl2)
+    u = u / (dist*dist)
+    Pnear = App.Vector(0, 0, 0)
+    Pnear.x = Pl1.x + u * (Pl2.x - Pl1.x)
+    Pnear.y = Pl1.y + u * (Pl2.y - Pl1.y)
+    Pnear.z = Pl1.z + u * (Pl2.z - Pl1.z)
+    return Pnear
+
+def distance(p1, p2):
+    ''' calcuates distance between 2 points given as App.vectors'''
+    dist = sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2)
+    return dist
